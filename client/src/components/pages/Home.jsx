721b4308f793b3bb1ca32e38fb184e374c910b98
Chris Cardelle <chris.cardelle@gmail.com>
2019-06-10 09:19:29 -0400

    Remopved unused styling

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -2,12 +2,6 @@ import React, { Component } from "react";
 import TrackList from "./TrackList";
 import api from "../../api";
 export default class Home extends Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //   }
-  // }
-
   render() {
     return (
       <div>
