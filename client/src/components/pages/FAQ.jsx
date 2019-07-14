a4d2e9b2b23601feecbb9a5bbcf3b49412f99d90
Chris Cardelle <chris.cardelle@gmail.com>
2019-07-14 12:21:59 -0400

    More clean-up

--- client/src/components/pages/FAQ.jsx
+++ client/src/components/pages/FAQ.jsx
@@ -1,12 +1,6 @@
 import React, { Component } from "react";
 
 class FAQ extends Component {
-  // constructor(props) {
-  //   super(props)
-  //   this.state = {
-  //   }
-  // }
-
   render() {
     return (
       <div className="container">
